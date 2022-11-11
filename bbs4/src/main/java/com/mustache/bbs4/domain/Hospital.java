fc9035aa84299654f9436b8e5464a5d4a453cc83
simhyunbo <hbo9347@naver.com>
2022-11-11 13:20:30 +0900

    hospital

--- bbs4/src/main/java/com/mustache/bbs4/domain/Hospital.java
+++ bbs4/src/main/java/com/mustache/bbs4/domain/Hospital.java
@@ -1,14 +1,27 @@
 package com.mustache.bbs4.domain;
 
+import lombok.Getter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
+@Getter
 @Table(name = "nation_wide_hospitals")
 public class Hospital {
     @Id
     private Integer id; // Long --> bigint
+
+    @Column(name = "road_name_address")
+    private String roadNameAddress;
+
+    @Column(name = "hospital_name")
+    private String hospitalName;
+    private Integer patientRoomCount;
+    private Integer totalNumberOfBeds;
+    private Float totalAreaSize;
+
 
 }
--- bbs4/src/test/java/com/mustache/bbs4/controller/HospitalControllerTest.java
+++ bbs4/src/test/java/com/mustache/bbs4/controller/HospitalControllerTest.java
@@ -1,7 +1,25 @@
 package com.mustache.bbs4.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.mustache.bbs4.domain.Hospital;
+import com.mustache.bbs4.repository.HospitalRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
 class HospitalControllerTest {
 
+    @Autowired
+    HospitalRepository hospitalRepository;
+
+    @Test
+    void findById() {
+        Optional<Hospital> hospital = hospitalRepository.findById(1);
+        Hospital hp = hospital.get();
+        System.out.println(hp.getId());
+        assertEquals(1, hp.getId());
+    }
 }
